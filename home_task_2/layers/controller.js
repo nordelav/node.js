d04bbaf55fd9fa863995605d1407b8b4296a7044
nordelav <antother.sad.world@gmail.com>
2025-05-31 22:14:49 +0300

    Crud logic has been added

--- home_task_2/layers/controller.js
+++ home_task_2/layers/controller.js
@@ -1,9 +1,28 @@
 const service = require("./service");
 const router = require("../helpers/router");
 
-router.addRoute("put", "/", async (req, res) => {
+router.addRoute("GET", "/", async (req, res) => {
   try {
-    const data = await service.getUser(1);
+    const data = await service.showUsers();
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify(data));
+  } catch (error) {
+    res.writeHead(500, { "Content-Type": "application/json" });
+    res.end(JSON.stringify({ error: "Internal Server Error" }));
+  }
+});
+
+
+router.addRoute("PUT", "/user/:id", async (req, res) => {
+   const userId = parseInt(req.params.id);
+  try {
+
+     if (isNaN(userId) || userId <= 0) {
+      res.writeHead(400, { "Content-Type": "application/json" });
+      return res.end(JSON.stringify({ error: "Invalid user ID" }));
+    }
+
+    const data = await service.getUser(userId);
     res.writeHead(200, { "Content-Type": "application/json" });
     res.end(JSON.stringify(data));
   } catch (error) {
